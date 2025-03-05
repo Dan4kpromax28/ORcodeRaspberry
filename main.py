dd93b325c9ae49c6b6e93f28f4ca237d70d0540b
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2025-03-05 15:09:03 +0200

    added part of code with check

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyzbar.pyzbar import decode
 from picamera2 import Picamera2, Preview
 from libcamera import controls
 from libcamera import Transform
-
+import time
 
 picam2 = Picamera2()
 picam2.start_preview(Preview.QTGL)
@@ -15,11 +15,29 @@ picam2.set_controls({"AfMode": 2, "AfTrigger": 0})
 
 barcodes = []
 correctCode = ""
+delay = 2.5
+delayAfter = 4
+lastCode = None
 
 while True:
     captureRGB = picam2.capture_array("main")
     barcodes = decode(captureRGB)
     if barcodes: 
-        for character in barcodes:
-            correctCode = character.data.decode("utf-8")
-    print(f"Kods ir {correctCode}")
+        correctCode = barcodes[0].data.decode("utf-8")
+        if lastCode != correctCode:
+            print(f"Jaunais kods ir: {correctCode}")
+            time.sleep(delay)
+            
+            captureRGB = picam2.capture_array("main")
+            newBarcodes = decode(captureRGB)
+            
+            if newBarcodes and newBarcodes[0].data.decode("utf-8") == correctCode:
+                print(f"Kods: {correctCode} ir pareizs")
+                time.sleep(delayAfter)
+            else:
+                print(f"notika kluda")
+                
+        
+                    
+            
+            
